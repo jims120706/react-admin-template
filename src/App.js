0698f7c7c17bb64b9737ff47a7cd574fc41eaf22
jims120706 <740767260@qq.com>
2020-02-04 10:42:05 +0800

    feat：添加路由

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Layout from './components/Layout';
 import './App.css';
 import BaseHeader from './components/Header';
+import { HashRouter, Route, Switch  } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			<BaseHeader />
-			<Layout />
-		</div>
+		<HashRouter>
+			<div className="App">
+				<BaseHeader />
+				<Layout />
+			</div>
+		</HashRouter>
 	);
 }
 
--- src/components/Header/Menu/index.js
+++ src/components/Header/Menu/index.js
@@ -1,41 +1,50 @@
 import React from 'react';
 import style from './index.module.scss';
 import { Icon } from 'antd';
+import { Link  } from 'react-router-dom';
 
 export default class Menu extends React.Component {
 	render() {
 		const menus = [
 			{
 				title: '玩家积分',
-				icon: 'contacts'
+				icon: 'contacts',
+				router: 'points'
 			},
 			{
 				title: '赔率限额',
-				icon: 'dollar'
+				icon: 'dollar',
+				router: 'pay'
 			},
 			{
 				title: '收码规则',
-				icon: 'code'
+				icon: 'code',
+				router: 'rule'
 			},
 			{
 				title: '追码定制',
-				icon: 'edit'
+				icon: 'edit',
+				router: 'edit'
 			},
 			{
 				title: '开盘封盘',
-				icon: 'unlock'
+				icon: 'unlock',
+				router: 'unlock'
 			},
 			{
 				title: '开奖信息',
-				icon: 'mail'
+				icon: 'mail',
+				router: 'message'
 			},
 			{
 				title: '流水统计',
-				icon: 'pie-chart'
+				icon: 'pie-chart',
+				router: 'sum'
 			},
 			{
 				title: '机器人已开启',
-				icon: 'sound'
+				icon: 'sound',
+				router: 'machine'
 			}
 		];
 		return (
@@ -43,10 +52,10 @@ export default class Menu extends React.Component {
 			<div className={style.menu}>
 				{menus.map((item, i) => {
 					return (
-						<div key={i} className={style['menu-item']}>
+						<Link key={i} className={style['menu-item']} to={item.router}>
 							<Icon type={item.icon} className={style['icon']} theme="twoTone"/>
 							{item.title}
-						</div>
+						</Link>
 					);
 				})}
 			</div>
